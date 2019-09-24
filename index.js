9f70226fcb639e2215dde78a7a00f1dc0c7eeaf9
saja amer <sajaamer86@gmail.com>
2019-09-24 16:09:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 let num1=1;
 let num2=62;
 let multiply=num1*num2;
-let random=math.floor(math.random()*1000)+1
-let mod=16%6
-let max=math.max(1,2,3,20)
+const random=math.floor(math.random()*1000)+1
+const mod=16%6
+const max=math.max(1,2,3,20)
